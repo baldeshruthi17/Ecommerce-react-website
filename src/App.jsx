224cab8634a7fa9fb9aa863236d73e7aad33f966
Student <Student@gmail.com>
2026-09-06 14:56:20 +0530

    Added auth

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,29 @@
-import React from 'react'
+import React from "react";
 import Home from "./pages/Home";
-import './App.css';
+import "./App.css";
 import Auth from "./pages/Auth";
 import Checkout from "./pages/Checkout";
-import {Route, Routes} from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import Navbar from "./components/Navbar";
+import AuthProvider  from "./context/AuthContext";
+import CartProvider from "./context/CartContext";
 
 function App() {
-  return(
-    <div className="app">
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home/>}/>
-        <Route path="/auth" element={<Auth/>}/>
-        <Route path="/checkout" element={<Checkout/>}/>
-      </Routes>
-    </div>
-  )
+    return (
+        <AuthProvider>
+            <CartProvider>
+            <div className="app">
+                <Navbar />
+
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/auth" element={<Auth />} />
+                    <Route path="/CartProvider" element={<Checkout />} />
+                </Routes>
+            </div>
+            </CartProvider>
+        </AuthProvider>
+    );
 }
 
-export default App
+export default App;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,20 +1,42 @@
-import {Link} from "react-router-dom";
-export default function Navbar(){
-    return(
-        <nav className="navbar">
-           <div className="navbar-container">
-            <Link to="/" className="navbar-brand">ShopHub</Link>
-            <div className="navbar-links">
-                <Link to="/" className="navbar-link">Home</Link>
-                <Link to="/Checkout" className="navbar-link">Cart</Link>
+import { Link } from "react-router-dom";
+import { useAuth } from "../context/AuthContext";
+
+export default function Navbar() {
+  const { user, logout } = useAuth();
+  return (
+    <nav className="navbar">
+      <div className="navbar-container">
+        <Link to="/" className="navbar-brand">
+          ShopHub
+        </Link>
+        <div className="navbar-links">
+          <Link to="/" className="navbar-link">
+            Home
+          </Link>
+          <Link to="/CartProvider" className="navbar-link">
+            Cart
+          </Link>
+        </div>
+        <div className="navbar-auth">
+          {!user ? (
+            <div className="navbar-auth-links">
+              <Link to="/auth" className="btn btn-secondary">
+                Login
+              </Link>
+              <Link to="/auth" className="btn btn-primary">
+                Signup
+              </Link>
             </div>
-            <div className="navbar-auth">
-                <div className="navbar-auth-links">
-                <Link to="/Auth" className="btn btn-secondary">Login</Link>
-                <Link to="/Auth"  className="btn btn-primary">Signup</Link>
-                </div>
+          ) : (
+            <div className="navbar-user">
+              <span className="navbar-greeting">Hello, {user.email}</span>
+              <button className="btn btn-secondary" onClick={logout}>
+                Logout
+              </button>
             </div>
-           </div>
-        </nav>
-    );
+          )}
+        </div>
+      </div>
+    </nav>
+  );
 }
